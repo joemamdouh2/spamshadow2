7ef088e48afc05cf0551be7ade4cd961d6186606
joemamdouh2 <39736521+joemamdouh2@users.noreply.github.com>
2018-11-28 20:29:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "512693975363223573"; // ايدي السررفر
+var channel = "513991573772697602";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
